bf6311c735910b3ab3c44d82530a980730b3c659
Ильгар Амирасланов <kagenetzu@gmail.com>
2020-11-19 00:29:42 +0800

    initial commit

---
+++ Drinks.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+
+
+    class Drink
+    {
+        public int volume = 0;//объем
+        public static Random rnd = new Random();
+
+        public virtual String GetInfo()
+        {
+            var str = String.Format($"\nОбъем(литр): {this.volume}");
+            return str;
+        }
+    }
+    //сок
+    public enum FruitType { lemon, watermelon, melon };
+    class Juice : Drink
+    {
+        public FruitType fruit = FruitType.lemon ;//фрукт
+        public bool fruitPieces;//мякоть
+
+        public override String GetInfo()
+        {
+            var str = "Сок";
+            str += base.GetInfo();
+            str += String.Format($"\nС фруктом: {this.fruit}");
+            str += String.Format($"\nНаличие мякоти: {this.fruitPieces}");
+            return str;
+        }
+        public static Juice Generate()
+        {
+            return new Juice
+            {
+                volume = rnd.Next(1,5), 
+                fruit = (FruitType)rnd.Next(3), 
+                fruitPieces = rnd.Next() % 2 == 0 
+            };
+        }
+    }
+    //газировка
+    public enum SodaType { pepsi, sprite, mirinda};
+    class Soda : Drink
+    {
+        
+        public SodaType soda = SodaType.pepsi;
+        public int numBubbles = 0; // кол-во пузырьков
+
+        public override String GetInfo()
+        {
+            var str = "Газировка";
+            str += base.GetInfo();
+            str += String.Format($"\nТип: {this.soda}");
+            str += String.Format($"\nКол-во пузырьков: {this.numBubbles}");
+            return str;
+        }
+        public static Soda Generate()
+        {
+            return new Soda
+            {
+                volume = rnd.Next(1, 5),
+                soda = (SodaType)rnd.Next(3),
+                numBubbles = rnd.Next() % 101
+            };
+        }
+    }
+    //алкоголь
+    public enum AlcoholType { vodka, beer, cognac };
+    class Alcohol : Drink
+    {
+        public AlcoholType alcohol = AlcoholType.vodka;
+        public int strength = 0; //крепость
+
+        public override String GetInfo()
+        {
+            var str = "Алкоголь";
+            str += base.GetInfo();
+            str += String.Format($"\nТип: {this.alcohol}");
+            str += String.Format($"\nКрепость: {this.strength}");
+            return str;
+        }
+        public static Alcohol Generate()
+        {
+            var test = new Alcohol {
+                volume = rnd.Next(1, 5),
+                alcohol = (AlcoholType)rnd.Next(3),
+                strength = 5
+            };
+            if (test.alcohol == AlcoholType.vodka)
+            {
+                test.strength = rnd.Next(37, 41);
+            }
+            else if (test.alcohol == AlcoholType.beer)
+            {
+                test.strength = rnd.Next(3, 8);
+            }
+            else
+            {
+                test.strength = rnd.Next(43, 50);
+            }
+            return test;
+        }
+        
+    }
+
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB4
+{
+    public partial class Form1 : Form
+    {
+        List<Drink> drinksList = new List<Drink>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.drinksList.Clear();
+            var rnd = new Random();
+            for (var i = 0; i < 15; ++i)
+            {
+                switch (rnd.Next() % 3) 
+                {
+                    case 0: 
+                        this.drinksList.Add(Juice.Generate());
+                        break;
+                    case 1: 
+                        this.drinksList.Add(Soda.Generate());
+                        break;
+                    case 2: 
+                        this.drinksList.Add(Alcohol.Generate());
+                        break;
+                        
+                }
+            }
+            ShowInfo();
+        }
+
+        private void ShowInfo()
+        {
+            int juiceCount = 0;
+            int sodaCount = 0;
+            int alcoholCount = 0;
+
+            foreach (var drink in this.drinksList)
+            {
+                if (drink is Juice)
+                {
+                    juiceCount += 1;
+                }
+                else if (drink is Soda)
+                {
+                    sodaCount += 1;
+                }
+                else if (drink is Alcohol)
+                {
+                    alcoholCount += 1;
+                }
+            }
+
+            richTextBox1.Text = $"Сок: {juiceCount}\nГазировка: {sodaCount}\nАлкоголь: {alcoholCount}\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.drinksList.Count == 0)
+            {
+                richTextBox2.Text = "В автомате нет напитков.";
+                return;
+            }
+
+            var drink = this.drinksList[0];
+            this.drinksList.RemoveAt(0);
+
+            richTextBox2.Text = drink.GetInfo();
+
+            ShowInfo();
+        }
+    }
+}
